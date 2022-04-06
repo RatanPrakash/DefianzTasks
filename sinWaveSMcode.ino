d6ffe2b37e92b33edd7a124ef45a2b730b2eb98c
RatanPrakash <ratanprakash892@gmail.com>
2022-04-06 23:17:26 +0530

    finalising code

--- sinWaveSMcode.ino
+++ sinWaveSMcode.ino
@@ -9,7 +9,7 @@ float curr_angle = 0; // current steer state of wheels
 float target_angle = 0;  // target angle to achieve by steering the wheels (Publishes by ros topics)
 
 // for conversion from deg to radians
-const float Pi = 3.14159; 
+const float Pi = 3.14159;    
 const float oneDegtoRad = Pi/180;
 
 
@@ -18,23 +18,25 @@ const float Nema23constAng = 0.9;        //0.9 degrees step
 const int microStep = 8;                 //from microstep driver settings
 
 
-void steer(int target_angle = 0){
+void steer(int target_angle){
   float steerAngle = target_angle - curr_angle; // difference of the current and target angle that needs to be executed. 
   
   if (steerAngle >= 0){
     digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction (Right)
   }
-  else {
+  else if (steerAngle < 0) {
     digitalWrite(dirPin, LOW); // (Left)
+    steerAngle = -steerAngle;
   }
     
   for(int x = 0; x < (microStep * (steerAngle/Nema23constAng)) ; x++){
     digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(100); 
+    delayMicroseconds(800); 
     digitalWrite(stepPin,LOW); 
-    delayMicroseconds(100); 
+    delayMicroseconds(800); 
  }
  curr_angle = target_angle; //setting the current angle to the Angle just steered...
+ Serial.println(curr_angle); 
 }
 
 void setup() {
@@ -48,12 +50,13 @@ void setup() {
 }
 
 void loop() {
+  
   for(int i = 0; i <= 360; i++){
     float Angle = sin(i * oneDegtoRad);                        //using sin func by converting angles to radian
-    float argAngle = map(Angle * 10, -10, 10, -45.0, 45.0);    // mapping extreme values of sin function to -45/+45 
+    float argAngle = map(Angle * 10, -10, 10, -30, 30);    // mapping extreme values of sin function to -45/+45 
     steer(argAngle);                                           // steering the wheels to the angle mapped 
-    Serial.println(curr_angle);                                //printing current angle of wheels to Plotter/Monitor
-    delay(125);
+                                   //printing current angle of wheels to Plotter/Monitor
+    delay(1);
 
   }
 
